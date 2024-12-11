877ea19581724e3329dbfcf51428fec31c4c9080
nischaladhikari0 <nischaladhikari609@gmail.com>
2024-12-11 16:04:50 +0545

    added swapping value

---
+++ Swapping value/Swapping value/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swapping_value
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5, b = 10;
+            Console.WriteLine("Before swap a= " + a + " b= " + b);
+            a = a + b;      
+            b = a - b;     
+            a = a - b;
+            Console.Write("After swap a= " + a + " b= " + b);
+        }
+    }
+}
